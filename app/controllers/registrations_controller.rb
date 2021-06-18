626f901feeabbe564369fcc8230f9b186c5f9123
DenisRem <ihor.moskvun5@gmail.com>
2021-06-18 16:03:14 +0300

    Fix stylesheet

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class RegistrationsController < ApplicationController
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name])
+  end
+  add_flash_types :danger, :info, :warning, :success
+end
