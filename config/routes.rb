9502f401e49140a365f8aa04a19fbd763b28a86e
DenisRem <ihor.moskvun5@gmail.com>
2021-06-22 11:24:52 +0300

    Custom error pages

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'home#index', as: 'home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/404', to: 'errors#not_found'
+  get '/422', to: 'errors#unacceptable'
+  get '/500', to: 'errors#server_error'
 end
