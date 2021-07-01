ba776e424e6e5a802ed174f6065945a629e21227
DenisRem <ihor.moskvun5@gmail.com>
2021-07-01 11:21:15 +0300

    Fix helper

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -4,7 +4,5 @@ module UserHelper
   def user_avatar_tag(user)
     avatar = user.avatar? ? user.avatar.url : 'default_a.png'
     image_tag(avatar, alt: 'Image', class: 'user-avatar')
-
-    image_tag(user.avatar.url, alt: 'Image', class: 'user-avatar')
   end
 end
